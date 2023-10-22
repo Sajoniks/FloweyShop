73b31a5b332e77e70397ccc68f58c464351e6858
Sajoniks <avesom33@mail.ru>
2023-10-22 15:01:54 +0300

    missing project files

---
+++ Flowey.Services.ProductAPI/Migrations/20230930171326_SeedProducts.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Flowey.Services.ProductAPI.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "CategoryName", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Цветы", "Розы колючие", "", "Розы", 100.0 },
+                    { 2, "Цветы", "Кросивый", "", "Лилии", 150.0 },
+                    { 3, "Комнатные растения", "Комнатные растения", "", "Фикус", 500.0 },
+                    { 4, "Комнатные растения", "Домашнее с большими листьями", "", "Монстера", 1500.0 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4);
+        }
+    }
+}
---
+++ Flowey.Services.ProductAPI/Program.cs
@@ -0,0 +1,39 @@
+using Flowey.Services.ProductAPI;
+using Flowey.Services.ProductAPI.DbContexts;
+using Flowey.Services.ProductAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(ops =>
+{
+    ops.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); 
+});
+
+builder.Services.AddMappingConfig(MappingConfigExtensions.RegisterMaps);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
